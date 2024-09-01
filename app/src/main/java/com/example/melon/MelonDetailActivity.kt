84a7e6b093f9114a9005b3dd8d8669d8961802ae
soyeon <sykim0659@naver.com>
2024-09-01 23:04:36 +0900

    melon 4

--- app/src/main/java/com/example/melon/MelonDetailActivity.kt
+++ app/src/main/java/com/example/melon/MelonDetailActivity.kt
@@ -2,17 +2,37 @@ package com.example.melon
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MelonDetailActivity : AppCompatActivity() {
+
+    lateinit var playPauseButton : ImageView
+    var is_playing: Boolean = false
+        set(value) {
+            if (value == true) {
+                playPauseButton.setImageDrawable(
+                    this.resources.getDrawable(R.drawable.pause, this.theme)
+                )
+            }else {
+                playPauseButton.setImageDrawable(
+                    this.resources.getDrawable(R.drawable.play, this.theme))
+            }
+            field = value
+        }
+    lateinit var melonItemList: ArrayList<MelonItem>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_melon_detail)
 
+        playPauseButton = findViewById(R.id.play)
+        playPauseButton.setOnClickListener{
+            if(is_playing == true) is_playing = false
+            else is_playing = true
+        }
         val melonItemList = intent.getSerializableExtra("melon_item_list") as ArrayList<MelonItem>
         melonItemList.forEach{
         Log.d("melonn", it.song)}
